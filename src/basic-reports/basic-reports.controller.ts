d729f3c125060f719089b6d2d21ed37efa19ce2e
Luciano Figueroa <lumusitech@gmail.com>
2025-08-01 13:46:25 -0300

    feat: add base reports module

---
+++ src/basic-reports/basic-reports.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { BasicReportsService } from './basic-reports.service';
+
+@Controller('basic-reports')
+export class BasicReportsController {
+  constructor(private readonly basicReportsService: BasicReportsService) {}
+
+  @Get()
+  hello() {
+    return this.basicReportsService.hello();
+  }
+}
