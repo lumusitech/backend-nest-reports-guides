de5f1d250597389ac527acb4b0caf23c0c76ce34
Luciano Figueroa <lumusitech@gmail.com>
2025-08-03 12:48:19 -0300

    feat: add other table with totals

--- src/reports/countries.report.ts
+++ src/reports/countries.report.ts
@@ -29,10 +29,14 @@ export const getCountryReport = (
           // headers are automatically repeated if the table spans over multiple pages
           // you can declare how many rows should be treated as headers
           headerRows: 1,
-          widths: [50, 50, 50, '*', 'auto', '*'],
+          //? 6 Columns
+          widths: [50, 50, 80, '*', 'auto', '*'],
 
           body: [
+            //? Header
             ['ID', 'ISO2', 'ISO3', 'NAME', 'CONTINENT', 'LOCAL NAME'],
+
+            //? Dynamic Content
             ...countries
               .slice(0, 30)
               .map((country) => [
@@ -43,10 +47,63 @@ export const getCountryReport = (
                 country.continent ?? '',
                 country.local_name ?? '',
               ]),
+
+            //? Added to add a new separation line within table to then, give totals and others
+            ['', '', '', '', '', ''],
+
+            //? Table Footer - each table with 6 columns
+            //! Warning: give content or empty string for each column
+            [
+              //? Column 1
+              '',
+              //? Column 2
+              '',
+              //? Column 3
+              '',
+              //? Column 4
+              '',
+              //? Column 5
+              'TOTAL:',
+              //? Column 6
+              { text: `${countries.length} countries`, bold: true },
+            ],
+          ],
+        },
+      },
+      // Totals table
+      {
+        text: 'Totals',
+        style: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 40, 0, 0],
+        },
+      },
+      {
+        layout: 'noBorders',
+        table: {
+          headerRows: 1,
+          widths: [50, 50, 80, '*', 'auto', '*'], //? 6 columns
+          body: [
+            [
+              //? Column 1
+              { text: 'Total of countries', colSpan: 2, bold: true },
+              //? Column 2
+              {},
+              //? Column 3
+              { text: `${countries.length} countries`, bold: true },
+              //? Column 4
+              {},
+              //? Column 5
+              {},
+              //? Column 6
+              {},
+            ],
           ],
         },
       },
     ],
+
     footer: footerSection,
   };
 };
