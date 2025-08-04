daa00334a844acb12dc6d0da3c5c6412712d443b
Luciano Figueroa <lumusitech@gmail.com>
2025-08-03 23:00:07 -0300

    feat: add custom layouts

--- src/reports/countries.report.ts
+++ src/reports/countries.report.ts
@@ -1,5 +1,9 @@
 import { countries as Country } from '@prisma/client';
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import type {
+  ContentTable,
+  TableLayout,
+  TDocumentDefinitions,
+} from 'pdfmake/interfaces';
 import { footerSection, headerSection } from './sections';
 
 interface ReportOptions {
@@ -8,6 +12,19 @@ interface ReportOptions {
   countries: Country[];
 }
 
+// TODO: refactor, extract all custom layouts to other file
+const myCustomLayout: TableLayout = {
+  //? Remember: if necessary, add i or other name, it will take an automatic index. Same with node
+  fillColor: (rowIndex: number, node: ContentTable) => {
+    if (rowIndex === node.table.body.length - 1) return '#7b90be'; //? if last row add this color
+    if (rowIndex === 0) return '#7b90be';
+    else if (rowIndex % 2 === 0) return '#F3F3F3';
+    else return 'white';
+  },
+  vLineWidth: () => 0,
+  hLineColor: (columnIndex: number) => (columnIndex !== 1 ? '#CCCCCC' : ''),
+};
+
 export const getCountryReport = (
   options: ReportOptions,
 ): TDocumentDefinitions => {
@@ -15,16 +32,18 @@ export const getCountryReport = (
 
   return {
     pageOrientation: 'landscape',
+    pageMargins: [40, 110, 40, 60],
+
     header: headerSection({
       title: title ?? 'Countries Report',
       subtitle: subtitle ?? 'List of countries',
     }),
-    pageMargins: [40, 110, 40, 60],
+
     content: [
       {
         // layout: 'headerLineOnly', 'lightHorizontalLines', 'noBorders' - go to the doc
         // or define your own custom layout
-        layout: 'lightHorizontalLines',
+        layout: myCustomLayout,
         table: {
           // headers are automatically repeated if the table spans over multiple pages
           // you can declare how many rows should be treated as headers
@@ -49,9 +68,9 @@ export const getCountryReport = (
               ]),
 
             //? Added to add a new separation line within table to then, give totals and others
-            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''], //? 6 columns
 
-            //? Table Footer - each table with 6 columns
+            //? Table Footer - each row with 6 columns
             //! Warning: give content or empty string for each column
             [
               //? Column 1
@@ -65,12 +84,15 @@ export const getCountryReport = (
               //? Column 5
               'TOTAL:',
               //? Column 6
-              { text: `${countries.length} countries`, bold: true },
+              {
+                text: `${countries.length} countries`,
+                bold: true,
+              },
             ],
           ],
         },
       },
-      // Totals table
+      //? title of the next table
       {
         text: 'Totals',
         style: {
@@ -79,21 +101,36 @@ export const getCountryReport = (
           margin: [0, 40, 0, 0],
         },
       },
+      //? Other Table -> Totals table with only 1 row
       {
-        layout: 'noBorders',
+        layout: {
+          fillColor: (rowIndex: number) => {
+            if (rowIndex === 0) return '#7b90be';
+            else if (rowIndex % 2 === 0) return '#F3F3F3';
+            else return 'white';
+          },
+          vLineWidth: () => 0,
+          //? Remember: if necessary, add i or other name, it will take an automatic index.
+          hLineWidth: (i: number) => (i === 1 ? 0 : 1),
+        },
         table: {
           headerRows: 1,
           widths: [50, 50, 80, '*', 'auto', '*'], //? 6 columns
           body: [
             [
               //? Column 1
-              { text: 'Total of countries', colSpan: 2, bold: true },
+              {
+                text: 'TOTAL OF COUNTRIES',
+                colSpan: 3,
+                bold: true,
+                fontSize: 12,
+              },
               //? Column 2
               {},
               //? Column 3
-              { text: `${countries.length} countries`, bold: true },
-              //? Column 4
               {},
+              //? Column 4
+              { text: `${countries.length} countries`, bold: true },
               //? Column 5
               {},
               //? Column 6
@@ -103,7 +140,6 @@ export const getCountryReport = (
         },
       },
     ],
-
     footer: footerSection,
   };
 };
